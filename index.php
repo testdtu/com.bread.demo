d376067780e60a932aa09bf36e056c71ff986907
dtudor <dtudor@reference.be>
2014-03-04 11:33:40 +0100

    finished for first release

--- index.php
+++ index.php
@@ -10,8 +10,7 @@ and open the template in the editor.
     <title></title>
   </head>
   <body>
-    <?php
-    // put your code here
-    ?>
+    <h1>Bread products</h1>
+    <p>List of bread products</p>
   </body>
 </html>
